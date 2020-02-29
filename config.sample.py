0cc1c450fbc71b7e502b94eb5c63ab2c649412d1
joycehsu <m234onica@gmail.com>
2020-02-29 15:30:14 +0800

    urf8 -> utf8mb4

--- config.sample.py
+++ config.sample.py
@@ -6,7 +6,7 @@ FLASK_ENV = os.environ.get('FLASK_ENV')
 
 DEBUG = True
 TEMPLATES_AUTO_RELOAD = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{username}:{passwword}@{host}/{db_name}'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{username}:{passwword}@{host}/{db_name}?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 JSON_AS_ASCII = False
