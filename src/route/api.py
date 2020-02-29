33392085a50c06666bf0732f3e1d1ade2b582e6b
joycehsu <m234onica@gmail.com>
2020-02-29 11:52:57 +0800

    static assets 改到run.py

--- src/route/api.py
+++ src/route/api.py
@@ -1,19 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, g, redirect, flash, url_for
-from uuid import uuid1
 from src.db import init_db, db_session
 from src.models import Catalogs
 
 api = Blueprint('api', __name__)
 init_db()
-
-@api.context_processor
-def utility_processor():
-  def setuuid(static):
-    return static + "?v=" + g.uuid
-  return {"setuuid": setuuid}
-
-
-@api.before_request
-def before_req():
-  g.uuid = str(uuid1())
-
