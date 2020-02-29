49bc923c4a378cc155eefe4ebc5cfc9335793b85
joycehsu <m234onica@gmail.com>
2020-02-29 15:53:57 +0800

    實作註冊功能，新增登入與註冊頁面

--- src/login/auth.py
+++ src/login/auth.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, flash, abort, redirect, render_template, url_for
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user
 
 from src import login_manager, bcrypt
-from src.models import User
-from src.login.form import LoginForm
+from src.models import Users
+from src.login.form import LoginForm, RegisterForm
 from src.db import init_db, db_session
 
 init_db()
@@ -11,35 +11,79 @@ init_db()
 
 @login_manager.user_loader
 def user_loader(id):
-    user = db_session.query(User).get(id)
+    user = db_session.query(Users).get(id)
     return user
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
     if current_user.is_authenticated:
         return redirect(url_for('blog.home'))
-    
-    form = LoginForm(request.form)
-    
+
+    form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+
+        db_session.add(Users(fullname=form.fullname.data, username=form.username.data,
+                            email=form.email.data, password=hashed_password))
+        db_session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+
+        return redirect(url_for('auth.login'))
+    else:
+        print('error:', form.errors)
+        return render_template('register.html', form=form)
+
+
+# @auth.route('/register', methods=['POST'])
+# def register():
+#     form = RegisterForm(request.form)
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(
+#             form.password.data).decode('utf-8')
+
+#         db_session.add(Users(username=form.username.data,
+#                             email=form.email.data, password=hashed_password))
+#         db_session.commit()
+#         flash('Your account has been created! You are now able to log in', 'success')
+#         return redirect(url_for('auth.login'))
+#     return render_template('register.html', form=form)
+
+
+@auth.route('/login', methods=['GET'])
+def login_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('blog.home'))
+
+    form = LoginForm(request.form)
+    return render_template('login.html', form=form)
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    form = LoginForm(request.form)
+
+    if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
 
-        user = db_session.query(User).filter_by(username=username).first()
-        print("user.password:", user.password)
-        print("password:", password)
-        if user:
-            login_user(user)
-
+        user = db_session.query(Users).filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user, remember=form.remember.data)
             next = request.args.get('next')
             return redirect(next) if next else redirect(url_for('blog.home'))
 
         else:
             flash('Logged in successful.')
+
     return render_template('login.html', form=form)
-    
-    return render_template('login.html', form=form)
+
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('blog.home'))
--- src/login/form.py
+++ src/login/form.py
@@ -1,9 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.fields import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, ValidationError, Length, Email, EqualTo
 
 from src.db import db_session
-from src.models import User
+from src.models import Users
+
+
+class RegisterForm(FlaskForm):
+    fullname = StringField('Fullname', validators=[
+                           DataRequired(message='Fullname is empty.')])
+    username = StringField('Username',
+                           validators=[DataRequired(message='Username is empty.'), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(message='Email is empty.'), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(message='Password is empty.'), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = db_session.query(Users).filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                'That username is taken. Please choose a defferent one.')
+
+    def validate_email(self, email):
+        user = db_session.query(Users).filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a defferent one.')
 
 
 class LoginForm(FlaskForm):
@@ -11,4 +36,5 @@ class LoginForm(FlaskForm):
                            DataRequired(message='username is empty.')])
     password = StringField('Password', validators=[
                            DataRequired(message='password is wrong.')])
+    remember = BooleanField('Remember me')
     submit = SubmitField('submit')
