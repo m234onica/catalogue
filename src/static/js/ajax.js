39043c48e8ac9afe859bb342e872130a6e109ad7
joycehsu <m234onica@gmail.com>
2020-03-01 22:43:19 +0800

    delete side menu and add navbar item

--- src/static/js/ajax.js
+++ src/static/js/ajax.js
@@ -1,10 +1,12 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
 
-  // Or with jQuery
+});
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+// Or with jQuery
+
+$(document).ready(function () {
+    $(".dropdown-trigger").dropdown({ constrainWidth: false, coverTrigger: false});
+});
+
